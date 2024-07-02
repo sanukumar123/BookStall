0496c208177ebf3ca49797eb56c9725dba55c52a
sanukumar123 <sanugupta4970@gmail.com>
2024-07-02 16:00:37 +0530

    Thora Bahoot aur kaam kiye Is Project me

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import Freebook from './components/Freebook'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Freebook />
       <Footer />
+      
     </>
   )
 }
