6ee79fd50d7ec842f1d711d1fe6bbcf687e9b26a
sanukumar123 <sanugupta4970@gmail.com>
2024-07-03 17:31:54 +0530

    Create Course Page and Do Routing

---
+++ Frontend/src/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Freebook from '../components/Freebook'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Freebook />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
